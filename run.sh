d470ad78ec39f518184015c68d18ebd095689d25
paul <paul__94@hotmail.com>
2022-06-03 23:05:23 +0200

    Add proper measurement results

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-docker build -t kotlin-async .
+# docker build -t kotlin-async . # --network common .
+mode="returned"
 cpus=4
-docker run -v $(pwd)/output:/app/output -e MAX=10000000000 -e SCALE=1000000000 -e MODES="shared" -e CPUS=$cpus --cpus=$cpus kotlin-async
+docker run -v $(pwd)/output:/app/output -e MAX=10000000000 -e SCALE=1000000000 -e MODES=$mode -e CPUS=$cpus -e CONCURRENT=1 --cpus=$cpus kotlin-async
